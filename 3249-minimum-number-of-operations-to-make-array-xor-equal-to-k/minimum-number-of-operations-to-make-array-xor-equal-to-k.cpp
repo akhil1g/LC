6df9441ba64e751a476d7ad9a9f7dd2379181e75
Akhil Gupta <66504875+akhil1g@users.noreply.github.com>
2024-04-29 21:27:18 +0530

    Time: 118 ms (37.00%) | Memory: 91.3 MB (54.80%) - LeetSync

--- 3249-minimum-number-of-operations-to-make-array-xor-equal-to-k/minimum-number-of-operations-to-make-array-xor-equal-to-k.cpp
+++ 3249-minimum-number-of-operations-to-make-array-xor-equal-to-k/minimum-number-of-operations-to-make-array-xor-equal-to-k.cpp
@@ -3,52 +3,16 @@ public:
     int minOperations(vector<int>& nums, int k) {
         int n=nums.size();
         int ans=0;
-        vector<int> bits(31,0);
         int x=0;
         for(int i=0;i<n;i++)
-        {
             x^=nums[i];
-        }
         for(int j=0;j<31;j++)
         {
             int b1=(x&(1<<j));
-                int b2=(k&(1<<j)); 
+            int b2=(k&(1<<j)); 
             if(b1!=b2)
-            {
                 ans++;
-            }
         }
         return ans;
-        for(int i=0;i<n;i++)
-        {
-            int ele=nums[i];
-            for(int j=0;j<31;j++)
-            {
-                int b1=(ele&(1<<j));
-                int b2=(k&(1<<j));
-                if(b2==1)
-                {
-                    if(b1==0)
-                    {
-                        ans++;
-                    }
-                }
-                else
-                {
-                    if(b1==1)
-                    {
-                        if(bits[j]==1)
-                        {
-                            continue;
-                        }
-                        else
-                        {
-                            ans++;
-
-                        }
-                    }
-                }
-            }
-        }
     }
 };
