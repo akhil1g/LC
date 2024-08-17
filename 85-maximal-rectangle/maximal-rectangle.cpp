83c08d5ab9a9cc2b3a150e968ab4dd5457f95dc1
Akhil Gupta <66504875+akhil1g@users.noreply.github.com>
2024-08-17 12:54:31 +0530

    Time: 192 ms (5.03%) | Memory: 21.7 MB (21.50%) - LeetSync

--- 85-maximal-rectangle/maximal-rectangle.cpp
+++ 85-maximal-rectangle/maximal-rectangle.cpp
@@ -1,35 +1,67 @@
 class Solution {
 public:
-    int fun(vector<int> &h)
-    {
+    int fun(vector<int> &heights){
         
-        stack<int> st1;
-        int n=h.size();
-        vector<int> prev(n,-1),next(n,n);
-        for(int i=n-1;i>=0;i--)
-        {
-            while(st1.size()&&h[st1.top()]>h[i])
-            {
-                prev[st1.top()]=i;
-                st1.pop();
-            }
-            st1.push(i);
-        }
-        stack<int> st2;
+        stack<int> st;
+        int n=heights.size();
         for(int i=0;i<n;i++)
-        {
-            while(st2.size()&&h[st2.top()]>h[i])
-            {
-                next[st2.top()]=i;
+        cout<<heights[i]<<" ";
+        cout<<"\n";
+        vector<int> nextsmaller(n,-1);
+        for(int i=n-1;i>=0;i--){
+            while(st.size()&&heights[i]<=heights[st.top()]){
+                st.pop();
+            }
+            if(st.size()){
+                nextsmaller[i]=st.top();
+            }
+            st.push(i);            
+        }
+        vector<int> prevsmaller(n,-1);
+        stack<int> st2;
+        for(int i=0;i<n;i++){
+            while(st2.size()&&heights[i]<=heights[st2.top()]){
                 st2.pop();
+            }
+            if(st2.size()){
+                prevsmaller[i]=st2.top();
             }
             st2.push(i);
         }
         int ans=0;
         for(int i=0;i<n;i++)
-        {
-            ans=max(ans,abs(next[i]-prev[i]-1)*h[i]);
+        cout<<prevsmaller[i]<<" ";
+        cout<<"\n";
+        for(int i=0;i<n;i++)
+        cout<<nextsmaller[i]<<" ";
+        cout<<"\n";
+        for(int i=0;i<n;i++){
+            int h=heights[i];
+            int temp=0;
+            if(h!=0){
+                if(prevsmaller[i]==-1&&nextsmaller[i]==-1){
+                    temp=h*n;
+                    ans=max(ans,h*n);
+                }
+                else
+                if(prevsmaller[i]!=-1&&nextsmaller[i]!=-1){
+                    temp=h*(nextsmaller[i]-prevsmaller[i]-1);
+                    ans=max(ans,h*(nextsmaller[i]-prevsmaller[i]-1));
+                }
+                else
+                if(prevsmaller[i]==-1){
+                    temp=h*(nextsmaller[i]+1);
+                    ans=max(ans,h*(nextsmaller[i]));
+                }
+                else
+                if(nextsmaller[i]==-1){
+                    temp=h*(n-prevsmaller[i]-1);
+                    ans=max(ans,h*(n-prevsmaller[i]-1));
+                }
+            }
+            cout<<temp<<" ";
         }
+        cout<<"\n";
         return ans;
     }
     int maximalRectangle(vector<vector<char>>& matrix) {
@@ -37,17 +69,18 @@ public:
         int m=matrix[0].size();
         int ans=0;
         vector<int> h(m,0);
-        for(int i=0;i<n;i++)
-        {
-            for(int j=0;j<m;j++)
-            {
-                if(matrix[i][j]=='0')
-                h[j]=0;
-                else
-                h[j]++;
+        for(int i=0;i<n;i++){
+            vector<int> heights(m,0);
+            for(int j=0;j<m;j++){
+                if(matrix[i][j]=='1'){
+                    heights[j]+=h[j]+1;
+                }
             }
-            ans=max(ans,fun(h));
-        }   
+            h=heights;
+            int temp=fun(heights);
+            cout<<temp<<"\n";
+            ans=max(ans,temp);
+        }
         return ans;
     }
 };
